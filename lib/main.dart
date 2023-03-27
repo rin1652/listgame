25836d7ceb7b655660692c2c4101792127516f3f
unknown <ndphuc@gmail.com>
2023-03-27 17:11:08 +0700

    2

--- lib/main.dart
+++ lib/main.dart
@@ -2,68 +2,115 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-        title: 'App đầu tiên của PHÚC',
-        home: SafeArea(
-          child: MyScaffold(),
-        )),
+    MaterialApp(
+      home: MyHomePage(
+        isLoading: false,
+        counter: 0,
+        child: MyCenterWidget(),
+      ),
+    ),
   );
 }
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
+class MyHomePage extends StatefulWidget {
+  final bool isLoading;
+  final int counter;
+  final Widget child;
 
-  final Widget title;
+  const MyHomePage({
+    required this.isLoading,
+    required this.counter,
+    required this.child,
+  });
+
+  @override
+  State<MyHomePage> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  late bool _isLoading;
+  late int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = widget.isLoading;
+    _counter = widget.counter;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: [
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.menu),
-            tooltip: 'Đây là menu',
-          ),
-          // lấp đầy khoảng trắng ở giữa
-          Expanded(child: title),
-          const IconButton(
-            onPressed: null,
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-          )
-        ],
+    print('rebuild MyHomePage');
+    return Scaffold(
+      body: MyInheritedWidget(
+        isLoading: _isLoading,
+        counter: _counter,
+        child: widget.child,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onFloatingButtonClicked,
+      ),
+    );
+  }
+
+  void onFloatingButtonClicked() {
+    print('Button clicked!. Call setState method');
+    setState(() {
+      _counter++;
+      if (_counter % 2 == 0) {
+        _isLoading = false;
+      } else {
+        _isLoading = true;
+      }
+    });
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild CounterWidget');
+    final myInheritedWidget = MyInheritedWidget.of(context);
+
+    if (myInheritedWidget == null) {
+      return Text('MyInheritedWidget was not found');
+    }
+
+    return myInheritedWidget.isLoading
+        ? CircularProgressIndicator()
+        : Text('${myInheritedWidget.counter}');
+  }
+}
+
+class MyCenterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild MyCenterWidget');
+    return Center(
+      child: CounterWidget(),
     );
   }
 }
 
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
+class MyInheritedWidget extends InheritedWidget {
+  final int counter;
+  final bool isLoading;
+  final Widget child;
+
+  MyInheritedWidget({
+    required this.isLoading,
+    required this.counter,
+    required this.child,
+  }) : super(child: child);
 
   @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Thanh bar nè',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .titleLarge,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, my name world!'),
-            ),
-          ),
-        ],
-      ),
-    );
+  bool updateShouldNotify(MyInheritedWidget oldWidget) {
+    return isLoading != oldWidget.isLoading || counter != oldWidget.counter;
+  }
+
+  static MyInheritedWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<MyInheritedWidget>();
   }
 }
